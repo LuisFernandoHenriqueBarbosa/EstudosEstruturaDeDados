117d5d7c7a4a79d9eaeb3e8a558e48907d686c64
Luis Fernando <luisfernandohenriquebarbosa18@hotmail.com>
2024-05-04 22:10:01 -0300

    updt2209

--- Lab4/Lab08_Luis_Fernando/src/CoordenadorExecucao.java
+++ Lab4/Lab08_Luis_Fernando/src/CoordenadorExecucao.java
@@ -4,7 +4,7 @@
 // Lab. : POLIMORFISMO e INTERFACES em JAVA
 // Ano/S: 2024/1S
 // ----------------------------------------
- 
+
 // classe principal (faz o papel efetivo de 'main',
 // sendo instanciada pelo programa principal do projeto)
 // -----------------------------------------------------
@@ -15,176 +15,193 @@ import java.util.Scanner;
 // classe coordenadora da execução dos testes
 public class CoordenadorExecucao {
 
-    // método que realiza todos os testes
-    public void executaTudo() {
+        // método que realiza todos os testes
+        public void executaTudo() {
 
-        // criação de um objeto 'Scanner' para a interação com o teclado
-        Scanner teclado = new Scanner(System.in);
+                // criação de um objeto 'Scanner' para a interação com o teclado
+                Scanner teclado = new Scanner(System.in);
 
-        // declaração de constantes para uso no menu
-        final int PROCESSAMENTO_INDIVIDUAL = 1;
-        final int PROCESSAMENTO_POLIMORFICO = 2;
-        final int EXIBICAO_NOME_CLASSE = 3;
-        final int SAIR = 9;
+                // declaração de constantes para uso no menu
+                final int PROCESSAMENTO_INDIVIDUAL = 1;
+                final int PROCESSAMENTO_POLIMORFICO = 2;
+                final int EXIBICAO_NOME_CLASSE = 3;
+                final int SAIR = 9;
 
-        // cria objetos das subclasses
-        EmpregadoAssalariado assalariado
-                = new EmpregadoAssalariado(
-                    "Carlos", 
-                    "Filho", 
-                    "111.111.111-11",
-                    13,
-                    8,
-                    1979,
-                    800.00);
+                // cria objetos das subclasses
+                EmpregadoAssalariado assalariado = new EmpregadoAssalariado(
+                                "Carlos",
+                                "Filho",
+                                "111.111.111-11",
+                                13,
+                                8,
+                                1979,
+                                800.00);
 
-        EmpregadoHorista horista
-                = new EmpregadoHorista(
-                    "Marcos", 
-                    "Neto", 
-                    "222.222.222-22",
-                    33,
-                    3,
-                    1991,
-                    16.75, 
-                    40);
+                EmpregadoHorista horista = new EmpregadoHorista(
+                                "Marcos",
+                                "Neto",
+                                "222.222.222-22",
+                                33,
+                                3,
+                                1991,
+                                16.75,
+                                40);
 
-        EmpregadoComissionado comissionado
-                = new EmpregadoComissionado(
-                        "Renato",
-                        "Sobrinho", 
-                        "333.333.333-33",
-                        5,
-                        13,
-                        1998,
-                        10000,
-                        .06);
+                EmpregadoComissionado comissionado = new EmpregadoComissionado(
+                                "Renato",
+                                "Sobrinho",
+                                "333.333.333-33",
+                                5,
+                                13,
+                                1998,
+                                10000,
+                                .06);
 
-        EmpregadoComissionadoMaisFixo comissionadoMaisFixo
-                = new EmpregadoComissionadoMaisFixo(
-                        "Robert",
-                        "Junior",
-                        "444.444.444-44",
-                        25,
-                        10,
-                        2001,
-                        5000,
-                        .04,
-                        300);
+                EmpregadoComissionadoMaisFixo comissionadoMaisFixo = new EmpregadoComissionadoMaisFixo(
+                                "Robert",
+                                "Junior",
+                                "444.444.444-44",
+                                25,
+                                10,
+                                2001,
+                                5000,
+                                .04,
+                                300);
 
-        // cria um array Employee de quatro elementos
-        Empregado empregados[] = new Empregado[4];
+                EmpregadoProducao empregadoProducao = new EmpregadoProducao(
+                                "Robert",
+                                "Junior",
+                                "444.444.444-44",
+                                25,
+                                10,
+                                2001,
+                                5.00,
+                                300);
 
-        // inicializa o array com Employees          
-        empregados[0] = assalariado;
-        empregados[1] = horista;
-        empregados[2] = comissionado;
-        empregados[3] = comissionadoMaisFixo;
+                // cria um array Employee de quatro elementos
+                Empregado empregados[] = new Empregado[5];
 
-        // declaração de variáveis a serem usadas (independentemente da Atividade)
-        int escolha;
+                // inicializa o array com Employees
+                empregados[0] = assalariado;
+                empregados[1] = horista;
+                empregados[2] = comissionado;
+                empregados[3] = comissionadoMaisFixo;
+                empregados[4] = empregadoProducao;
 
-        // menu de escolha da Atividade a ser executada
-        do {
-            // apresentação das opções de escolha
-            System.out.println("\n");
-            System.out.println("Escolha uma das opções abaixo:");
-            System.out.println("1 - Atividade 1: Processamento INDIVIDUAL  de vários tipos de objetos 'Empregado'");
-            System.out.println("2 - Atividade 2: Processamento POLIMÓRFICO de vários tipos de objetos 'Empregado'");
-            System.out.println("3 - Atividade 3: Exibição do nome da classe efetiva de cada objeto da classe 'Empregado'");
-            System.out.println("9 - Sair");
+                // declaração de variáveis a serem usadas (independentemente da Atividade)
+                int escolha;
 
-            System.out.print("\nDigite aqui a sua opção: ");
-            // coleta da opção digitada
-            escolha = teclado.nextInt();
-            teclado.nextLine();
+                // menu de escolha da Atividade a ser executada
+                do {
+                        // apresentação das opções de escolha
+                        System.out.println("\n");
+                        System.out.println("Escolha uma das opções abaixo:");
+                        System.out.println(
+                                        "1 - Atividade 1: Processamento INDIVIDUAL  de vários tipos de objetos 'Empregado'");
+                        System.out.println(
+                                        "2 - Atividade 2: Processamento POLIMÓRFICO de vários tipos de objetos 'Empregado'");
+                        System.out.println(
+                                        "3 - Atividade 3: Exibição do nome da classe efetiva de cada objeto da classe 'Empregado'");
+                        System.out.println(
+                                        "4 - Atividade 4: Exibição do nome da classe efetiva de cada objeto da classe 'EmpregadoProdução'");
+                        System.out.println("9 - Sair");
 
-            // avaliação da opção e acionamento da respectiva Atividade
-            switch (escolha) {
+                        System.out.print("\nDigite aqui a sua opção: ");
+                        // coleta da opção digitada
+                        escolha = teclado.nextInt();
+                        teclado.nextLine();
 
-                case PROCESSAMENTO_INDIVIDUAL:
-                    System.out.println("\nAtividade 1: Processamento INDIVIDUAL  de vários tipos de objetos 'Empregado'");
-                    System.out.println("-----------------------------------------------------------------------------");
+                        // avaliação da opção e acionamento da respectiva Atividade
+                        switch (escolha) {
 
-                    System.out.println("Empregados processados individualmente:\n");
+                                case PROCESSAMENTO_INDIVIDUAL:
+                                        System.out.println(
+                                                        "\nAtividade 1: Processamento INDIVIDUAL  de vários tipos de objetos 'Empregado'");
+                                        System.out.println(
+                                                        "-----------------------------------------------------------------------------");
 
-                    System.out.printf("%s\n%s: $%,.2f\n\n",
-                            assalariado, "Vencimentos", assalariado.vencimentos());
-                    System.out.printf("%s\n%s: $%,.2f\n\n",
-                            horista, "Vencimentos", horista.vencimentos());
-                    System.out.printf("%s\n%s: $%,.2f\n\n",
-                            comissionado, "Vencimentos", comissionado.vencimentos());
-                    System.out.printf("%s\n%s: $%,.2f\n\n",
-                            comissionadoMaisFixo,
-                            "Vencimentos", comissionadoMaisFixo.vencimentos());
+                                        System.out.println("Empregados processados individualmente:\n");
 
-                    break;
+                                        System.out.printf("%s\n%s: $%,.2f\n\n",
+                                                        assalariado, "Vencimentos", assalariado.vencimentos());
+                                        System.out.printf("%s\n%s: $%,.2f\n\n",
+                                                        horista, "Vencimentos", horista.vencimentos());
+                                        System.out.printf("%s\n%s: $%,.2f\n\n",
+                                                        comissionado, "Vencimentos", comissionado.vencimentos());
+                                        System.out.printf("%s\n%s: $%,.2f\n\n",
+                                                        comissionadoMaisFixo,
+                                                        "Vencimentos", comissionadoMaisFixo.vencimentos());
 
-                case PROCESSAMENTO_POLIMORFICO:
-                    System.out.println("\nAtividade 2: Processamento POLIMÓRFICO de vários tipos de objetos 'Empregado'");
-                    System.out.println("-----------------------------------------------------------------------------");
+                                        break;
 
-                    System.out.println("Empregados processados polimorficamente::\n");
+                                case PROCESSAMENTO_POLIMORFICO:
+                                        System.out.println(
+                                                        "\nAtividade 2: Processamento POLIMÓRFICO de vários tipos de objetos 'Empregado'");
+                                        System.out.println(
+                                                        "-----------------------------------------------------------------------------");
 
-                    // processa genericamente cada elemento no empregados
-                    for (Empregado empregadoAtual : empregados) {
-                        System.out.println(empregadoAtual); // invoca toString
+                                        System.out.println("Empregados processados polimorficamente::\n");
 
-                        // determina se elemento é um 'EmpregadoComissionadoMaisFixo'
-                        if (empregadoAtual instanceof EmpregadoComissionadoMaisFixo) {
-                            // 'downcast' da referência de 'Empregado' para
-                            // referência a 'EmpregadoComissionadoFixo'
-                            EmpregadoComissionadoMaisFixo empregado
-                                    = (EmpregadoComissionadoMaisFixo) empregadoAtual;
+                                        // processa genericamente cada elemento no empregados
+                                        for (Empregado empregadoAtual : empregados) {
+                                                System.out.println(empregadoAtual); // invoca toString
 
-                            double salarioBaseAnterior = empregado.getSalarioBase();
-                            empregado.setSalarioBase(1.10 * salarioBaseAnterior);
-                            System.out.printf(
-                                    "Novo salário base com 10%% de aumento é: $%,.2f\n",
-                                    empregado.getSalarioBase());
-                        } // fim do if
+                                                // determina se elemento é um 'EmpregadoComissionadoMaisFixo'
+                                                if (empregadoAtual instanceof EmpregadoComissionadoMaisFixo) {
+                                                        // 'downcast' da referência de 'Empregado' para
+                                                        // referência a 'EmpregadoComissionadoFixo'
+                                                        EmpregadoComissionadoMaisFixo empregado = (EmpregadoComissionadoMaisFixo) empregadoAtual;
 
-                        System.out.printf(
-                                "Vencimentos $%,.2f\n\n", empregadoAtual.vencimentos());
-                    } // for final
+                                                        double salarioBaseAnterior = empregado.getSalarioBase();
+                                                        empregado.setSalarioBase(1.10 * salarioBaseAnterior);
+                                                        System.out.printf(
+                                                                        "Novo salário base com 10%% de aumento é: $%,.2f\n",
+                                                                        empregado.getSalarioBase());
+                                                } // fim do if
 
-                    break;
+                                                System.out.printf(
+                                                                "Vencimentos $%,.2f\n\n", empregadoAtual.vencimentos());
+                                        } // for final
 
-                case EXIBICAO_NOME_CLASSE:
-                    System.out.println("\nAtividade 3: Exibição do nome da classe efetiva de cada objeto da classe 'Empregado'");
-                    System.out.println("--------------------------------------------------------------------------------------");
+                                        break;
 
-                    System.out.println("Nome da classe efetiva de cada Empregado no vetor 'empregados':");
+                                case EXIBICAO_NOME_CLASSE:
+                                        System.out.println(
+                                                        "\nAtividade 3: Exibição do nome da classe efetiva de cada objeto da classe 'Empregado'");
+                                        System.out.println(
+                                                        "--------------------------------------------------------------------------------------");
 
-                    // obtém o nome do tipo de cada objeto no array empregados
-                    for (int j = 0; j < empregados.length; j++) {
-                        System.out.printf("empregados[%d] é um %s\n", j,
-                                empregados[j].getClass().getName());
-                    }
+                                        System.out.println(
+                                                        "Nome da classe efetiva de cada Empregado no vetor 'empregados':");
 
-                    break;
+                                        // obtém o nome do tipo de cada objeto no array empregados
+                                        for (int j = 0; j < empregados.length; j++) {
+                                                System.out.printf("empregados[%d] é um %s\n", j,
+                                                                empregados[j].getClass().getName());
+                                        }
 
-                case SAIR:
-                    System.out.println("\nSair ...\n");
-                    break;
+                                        break;
 
-                default:
-                    System.out.println("\nOpção inválida !  Tente novamente ...\n");
-            }
+                                case SAIR:
+                                        System.out.println("\nSair ...\n");
+                                        break;
 
-        } while (escolha != SAIR);
+                                default:
+                                        System.out.println("\nOpção inválida !  Tente novamente ...\n");
+                        }
 
-        teclado.close();
+                } while (escolha != SAIR);
 
-        // aciona método para exibição dos dados do autor do trabalho
-        Autor autor = new Autor();
-        autor.exibeDadosAutor(
-                "ESTRUTURAS DE DADOS - 2024/1S",
-                "COMPOSIÇÃO e HERANÇA em JAVA",
-                "LUIS FERNANDO HENRIQUE BARBOSA",
-                "121 092 231 1 034");
+                teclado.close();
 
-    }
+                // aciona método para exibição dos dados do autor do trabalho
+                Autor autor = new Autor();
+                autor.exibeDadosAutor(
+                                "ESTRUTURAS DE DADOS - 2024/1S",
+                                "COMPOSIÇÃO e HERANÇA em JAVA",
+                                "LUIS FERNANDO HENRIQUE BARBOSA",
+                                "121 092 231 1 034");
+
+        }
 
 }
-
